dd6f2d988d1a043e847c7e8e35f24f0edd1ab77d
mesh16 <61237129+mesh16@users.noreply.github.com>
2020-03-13 16:15:16 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -6,15 +6,15 @@
 
                 <div class="col-md-6 text-center footer-content">
                     <p><?php bloginfo('name'); ?></p>
-                    <img src="http://http://206.189.45.97/~mesh16/wp-content/themes/josh_theme/images/tumblr.png" alt=“tumblr”>
-                    <img src="http://http://206.189.45.97/~mesh16/wp-content/themes/josh_theme/images/rss.png" alt=“rss”>
+                    <img src="http://206.189.45.97/~mesh16/wp-content/themes/josh_theme/images/tumblr.png" alt=“tumblr”>
+                    <img src="http://206.189.45.97/~mesh16/wp-content/themes/josh_theme/images/rss.png" alt=“rss”>
                 </div>
                 <!--col-md-6-->
 
                 <div class="col-md-6 text-center footer-content">
                     <p>Terms and Conditions</p>
-                    <img src="http://http://206.189.45.97/~mesh16/wp-content/themes/josh_theme/images/linkedin.png" alt=“linkedin”>
-                    <img src="http://http://206.189.45.97/~mesh16/wp-content/themes/josh_theme/images/fb.png" alt="facebook">
+                    <img src="http://206.189.45.97/~mesh16/wp-content/themes/josh_theme/images/linkedin.png" alt=“linkedin”>
+                    <img src="http://206.189.45.97/~mesh16/wp-content/themes/josh_theme/images/fb.png" alt="facebook">
                 </div>
             </div>
         </div>
