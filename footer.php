8d9ee1ce87e19d0a3083eb9243e27f50dc7421f1
mesh16 <61237129+mesh16@users.noreply.github.com>
2020-03-13 15:58:26 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,29 @@
+    <footer class="container-fluid">
+
+        <div class="container">
+
+            <div class="row">
+
+                <div class="col-md-6 text-center footer-content">
+                    <p><?php bloginfo('name'); ?></p>
+                    <img src="http://http://206.189.45.97/~mesh16/wp-content/themes/josh_theme/images/tumblr.png" alt=“tumblr”>
+                    <img src="http://http://206.189.45.97/~mesh16/wp-content/themes/josh_theme/images/rss.png" alt=“rss”>
+                </div>
+                <!--col-md-6-->
+
+                <div class="col-md-6 text-center footer-content">
+                    <p>Terms and Conditions</p>
+                    <img src="http://http://206.189.45.97/~mesh16/wp-content/themes/josh_theme/images/linkedin.png" alt=“linkedin”>
+                    <img src="http://http://206.189.45.97/~mesh16/wp-content/themes/josh_theme/images/fb.png" alt="facebook">
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+ 
+</body>
+
+</html>
+
+<?php wp_footer(); ?>
