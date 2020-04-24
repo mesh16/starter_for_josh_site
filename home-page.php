d438da859e8e198d83fc1372528a84c4928bbf05
mesh16 <61237129+mesh16@users.noreply.github.com>
2020-04-24 18:09:13 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -37,7 +37,7 @@
     <section class="container-fluid aboutbg text-center">
         <div class="container">
             <h2><?php the_field('titleforaboutme'); ?></h2><!--  custom field for the aboutme content-->
-            <p class="particle"><?php the_field('subtitleforaboutme'); ?></p>
+            <p class="particle animated fadeInLeft"><?php the_field('subtitleforaboutme'); ?></p>
             <p class="about-text"><?php the_field('paragraphforaboutme'); ?></p>
 
             <div class="blocker"></div>
@@ -55,7 +55,7 @@
                 <!-- a row that gives us access to the BS columns-->
 
                 <div class="article-title text-center">
-                <h3 class="text-center article-title "><?php the_field('titleforarticles'); ?></h3>
+                <h3 class="text-center article-title wow fadeInLeft"><?php the_field('titleforarticles'); ?></h3>
                 </div>
 
                 <!--col-md-4-->
